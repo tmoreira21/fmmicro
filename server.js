315107c5a34b1df87df9b6006b0c180a9d85e40e
Tiago Moreira <thmroot@gmail.com>
2017-04-04 15:18:27 +0000

    Final version

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var express = require('express');
-
 var app = express();
-
-
+var path = require('path');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 
 app.get('/',function(req,res){
-    
-	res.send("Hello World");
-
+	res.sendFile(path.join(__dirname + '/form.htm'));
 });
 
+app.get('/get-file-size', multipartMiddleware, function(req,res){
+    res.send('No file sent.');
+});
 
+app.post('/get-file-size', multipartMiddleware, function(req,res){
+    res.send('File size: ' + req.files.file.size.toString() + ' bytes');
+});
 
 app.listen(8080,function(){
-
     console.log('Listening on port 8080');
-
 });
